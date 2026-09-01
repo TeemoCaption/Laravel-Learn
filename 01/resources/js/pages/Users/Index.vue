b534a9fbfbee28ae0fcd3fd0f83e253351ff3507
TeemoCaption <eyan0816@gmail.com>
2026-09-01 14:45:56 +0800

    Fix Inertia pages and application routes

--- 01/resources/js/pages/Users/Index.vue
+++ 01/resources/js/pages/Users/Index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-
+import { Link } from '@inertiajs/vue3';
+import { show } from '@/routes/users';
 </script>
 
 <template>
@@ -8,5 +9,8 @@
         <h1>使用者列表</h1>
 
         <p>這是 Laravel + Inertia + Vue 的頁面。</p>
+
+        <!-- 使用產生的 users.show 路由前往指定使用者詳細資料頁 -->
+        <Link :href="show(1)">查看使用者 ID：1</Link>
     </main>
 </template>
